89bb34f92ef3e04f01dcdc9a4634de3256725f40
TheFenixx21 <lorenzoesparragoza279@gmail.com>
2026-05-19 09:06:04 -0400

    Agregados getters y setters

--- back/src/main/java/com/unexca/empleados/Empleados.java
+++ back/src/main/java/com/unexca/empleados/Empleados.java
@@ -9,9 +9,9 @@ import jakarta.persistence.Id;
 public class Empleado {
 
     @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    private Long id;
     private String primernombre;
     private String segundonombre;
     private String apellidopaterno;
@@ -21,4 +21,66 @@ public class Empleado {
     private String correo;
     private String direccion;
     
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrimernombre() {
+        return primernombre;
+    }
+    public void setPrimernombre(String primernombre) {
+        this.primernombre = primernombre;
+    }
+
+    public String getSegundonombre() {
+        return segundonombre;
+    }
+    public void setSegundonombre(String segundonombre) {
+        this.segundonombre = segundonombre;
+    }
+
+    public String getApellidopaterno() {
+        return apellidopaterno;
+    }
+    public void setApellidopaterno(String apellidopaterno) {
+        this.apellidopaterno = apellidopaterno;
+    }
+
+    public String getApellidomaterno() {
+        return apellidomaterno;
+    }
+    public void setApellidomaterno(String apellidomaterno) {
+        this.apellidomaterno = apellidomaterno;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Double getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(Double telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
